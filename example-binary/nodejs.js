10eb71bb1118ea22b3df449db58df664f852dc83
bytedream <bytedream@protonmail.com>
2025-03-03 16:49:07 +0100

    update example nodejs scripts

--- example-binary/nodejs.js
+++ example-binary/nodejs.js
@@ -1,5 +1,17 @@
 async function main() {
-    const wasm = await import('./target/wasm32-unknown-emscripten/debug/example-binary.js');
+    let wasm;
+    for (const buildType of ['release', 'debug']) {
+        try {
+            wasm = await import(`./target/wasm32-unknown-emscripten/${buildType}/example-binary.js`);
+            break;
+        } catch (e) {
+            if (e.code !== 'ERR_MODULE_NOT_FOUND') throw e;
+        }
+    }
+    if (!wasm) {
+        throw new Error('Rust wasm binary not built');
+    }
+
     const module = {
         print: (str) => console.log(str),
         printErr: (str) => console.error(str),
